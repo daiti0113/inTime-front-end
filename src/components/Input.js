24f03491df7e6b2e20afbbd9d87e2c4ee51a4254
daiti0113 <stennis.ds.0113@gmail.com>
2020-07-12 23:49:26 +0900

    feat:Added Form into Top

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,8 +1,8 @@
 import React from "react"
 
 export const Input = ({title, ...rest}) => (
-    <>
+    <div>
         {title && <label htmlFor={rest.id}>{title}</label>}
         <input {...rest} />
-    </>
+    </div>
 )
--- src/components/Top.js
+++ src/components/Top.js
@@ -5,24 +5,40 @@ import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css"
 
 const useStyles = createUseStyles({
+    rangeDataPicker:{
+        display: "grid",
+        gridTemplateColumns: "200px 200px"
+    },
+    form: {
+        display: "grid",
+        gridTemplateColumns: "500px 500px"
+    }
 })
 
 const RangeDataPicker = () => {
     const [startDate, setStartDate] = useState(new Date())
     const [endDate, setEndDate] = useState(new Date())
+    const classes = useStyles()
 
     return (
-        <>
-            <DatePicker selected={startDate} onChange={date => setStartDate(date)} selectsStart startDate={startDate} endDate={endDate} />
-            <DatePicker selected={endDate} onChange={date => setEndDate(date)} selectsEnd startDate={startDate} endDate={endDate} minDate={startDate} />
-        </>
+        <div className={classes.rangeDataPicker}>
+            <div>
+                <label htmlFor="startDate">Start Date</label>
+                <DatePicker selected={startDate} onChange={date => setStartDate(date)} selectsStart startDate={startDate} endDate={endDate} id="startDate" />
+            </div>
+            <div>
+                <label htmlFor="endDate">End Date</label>
+                <DatePicker selected={endDate} onChange={date => setEndDate(date)} selectsEnd startDate={startDate} endDate={endDate} minDate={startDate} id="endDate" />
+            </div>
+        </div>
     )
 }
+
 const Form = () => {
-    // const date = new Date()
+    const classes = useStyles()
 
     return (
-        <div>
+        <div className={classes.form}>
             <Input title="Task Name" type="text" placeholder="Make a cake" id="taskName" />
             <RangeDataPicker />
         </div>
