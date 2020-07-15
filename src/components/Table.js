b180c34f0818da2e903fdbee953b4fa4f6432101
daiti0113 <stennis.ds.0113@gmail.com>
2020-07-15 16:32:43 +0900

    style:Clean up code of Table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,37 +1,40 @@
-import React, {useState} from "react"
+import React, {useState, useEffect} from "react"
+import {fetchData} from "../helper/fetchData"
 
-const creatRow = (year, month) => {
-    const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
-    const endDate = new Date(year, month,  0) // 月の最後の日を取得
+const createRow = (year, month) => {
+    const startDate = new Date(year, month, 1) // 月の最初の日を取得
+    const endDate = new Date(year, month + 1,  0) // 月の最後の日を取得
     const endDayCount = endDate.getDate() // 月の末日
-    console.log(startDate, endDayCount)
+    console.log(startDate, endDate, endDayCount)
     
     // eslint-disable-next-line react/jsx-key
-    return [...Array(endDayCount)].map((_, i) => <th>{i}</th>)
+    return [...Array(endDayCount)].map((_, i) => <th>{i+1}</th>)
+}
 
-    // const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+const TableHeader = ({year, month}) => {
+    return (
+        <thead>
+            <tr>
+                <th>Task</th>
+                {createRow(year, month)}
+            </tr>
+        </thead>
+    )
 }
 
 export const Table = () => {
     const date = new Date()
     const [year, setYear] = useState(date.getFullYear())
     const [month, setMonth] = useState(date.getMonth())
+    const [tasks, setTasks] = useState([])
+
+    fetchData(useEffect, "/tasks", setTasks)
 
     return (
         <table>
-            <thead>
-                <tr>
-                    <th>Task</th>
-                    {creatRow(year, month)}
-                </tr>
-            </thead>
+            <TableHeader year={year} month={month} />
             <tbody>
-                <tr>
-                    <td>Make a Cake</td>
-                </tr>
-                <tr>
-                    <td>Eat a Cake</td>
-                </tr>
+                {console.log(tasks)}
             </tbody>
         </table>
     )
