43e463a6d6e1c980679f6cf4f0fd7ca0e01de10f
daiti0113 <stennis.ds.0113@gmail.com>
2020-07-24 00:54:56 +0900

    feat:Added month into dateHeader

--- src/components/Table.js
+++ src/components/Table.js
@@ -7,23 +7,54 @@ import {range} from "../helper/arrayManipulation"
 const useStyles = createUseStyles({
     table: ({colCount}) => ({
         display: "grid",
-        gridTemplateRows: `30px repeat(${colCount}, 30px) 1fr`,
+        gridTemplateRows: `60px repeat(${colCount}, 30px) 1fr`,
         marginTop: 50,
         width: ""
     }),
-    dateHeader: {
+    dateHeaderContainer: {
         display: "grid",
-        gridTemplateColumns: `200px 1fr`,
+        gridTemplateRows: "30px 1fr",
+        gridTemplateColumns: "200px 1fr",
         height: "500px",
         border: "1px solid"
     },
-    date: ({displayPeriod}) => ({
+    dateHeaderDayContainer: ({displayPeriod}) => ({
+        gridRow: "2 / 3",
+        gridColumn: "2 / 3",
         display: "grid",
         gridTemplateColumns: `repeat(${displayPeriod}, 20px)`,
         gridTemplateAreas: "title taskBar"
     }),
-    day: {
-        borderLeft: "1px solid"
+    dateHeaderDay: {
+        borderLeft: "1px solid",
+        borderTop: "1px solid",
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: 3
+    },
+    dateHeaderMonthContainer: ({displayPeriod}) => ({
+        gridRow: "1 / 2",
+        gridColumn: "2 / 3",
+        display: "grid",
+        gridTemplateColumns: `repeat(${displayPeriod}, 20px)`,
+    }),
+    dateHeaderMonth: () => ({
+        borderLeft: "1px solid",
+        display: "flex",
+        justifyContent: "center",
+        paddingTop: 3
+    }),
+    dateHeaderTaskContainer: {
+        gridRow: "1 / 3",
+        gridColumn: "1 / 2",
+        justifySelf: "center",
+        paddingTop: 20
+    },
+    ruledLine: {
+
+    },
+    taskListContainer: {
+        borderTop: "1px solid"
     },
 })
 
@@ -32,13 +63,22 @@ const DateHeader = ({displayStartDate, displayPeriod}) => {
     const classes = useStyles({displayPeriod})
 
     return (
-        <div className={classes.dateHeader}>
-            <div>Task</div>
-            <div className={classes.date}>
+        <div className={classes.dateHeaderContainer}>
+            <div className={classes.dateHeaderTaskContainer}>Task</div>
+            <div className={classes.dateHeaderMonthContainer}>
+                {/* TODO: don't use startDate */}
+                {range(startDate, displayPeriod).map((_, idx) => {
+                    displayStartDate.setDate(displayStartDate.getDate() + 1)
+                    return (idx === 0 || displayStartDate.getDate() === 1) 
+                        ? <div className={classes.dateHeaderMonth}>{displayStartDate.getMonth()}</div>
+                        : <div />
+                })}
+            </div>
+            <div className={classes.dateHeaderDayContainer}>
                 {/* TODO: don't use startDate */}
                 {range(startDate, displayPeriod).map(() => {
                     displayStartDate.setDate(displayStartDate.getDate() + 1)
-                    return (<div className={classes.day}>{displayStartDate.getDate()}</div>)
+                    return <div className={classes.dateHeaderDay}>{displayStartDate.getDate()}</div>
                 })}
             </div>
         </div>
@@ -59,7 +99,9 @@ export const Table = () => {
     return (
         <div className={classes.table}>
             <DateHeader displayStartDate={displayStartDate} displayPeriod={displayPeriod} />
-            {tasks && tasks.map((task, idx) => <TaskBar key={idx+1} row={idx + 1} startDate={task.startDate} endDate={task.endDate}>{task.taskName}</TaskBar>)}
+            <div className={classes.taskListContainer}>
+                {tasks && tasks.map((task, idx) => <TaskBar key={idx+1} row={idx + 1} startDate={task.startDate} endDate={task.endDate}>{task.taskName}</TaskBar>)}
+            </div>
         </div>
     )
 }
