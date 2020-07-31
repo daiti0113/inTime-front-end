1d3a5b2698e6ae39d6013340113f2bf193310fb5
daiti0113 <stennis.ds.0113@gmail.com>
2020-07-31 23:51:08 +0900

    fix:Fixed .eslintrc to enable ESLint on VSCode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,28 @@
+
 // eslint-disable-next-line no-undef
 module.exports = {
-    extends: ["eslint:recommended", "standard-react"],
-    plugins: ["import", "react"],
-    rules: {
-        "linebreak-style": "off",
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "linebreak-style": ["error","unix"],
+        "quotes": ["error","double"],
+        "semi": ["error","never"],
         "no-unused-vars": 2,
         "react/jsx-uses-vars": 2,
         "react/jsx-indent": ["error", 4],
@@ -11,7 +30,6 @@ module.exports = {
         "react/prop-types": [0],
         "jsx-quotes": ["warn", "prefer-double"],
         "indent": ["error", 4, {"SwitchCase": 1}],
-        "quotes": ["warn","double"],
         "max-lines-per-function": ["error", 20],
         "max-statements": ["error", 15],
         "max-depth": ["error", 2],
@@ -20,18 +38,6 @@ module.exports = {
         "no-var": "error",
         "block-spacing": ["error", "never"],
         "object-curly-spacing": ["error", "never"],
-        "semi": ["error", "never"],
         "comma-dangle": ["error", "never"]
-    },
-    parser: "babel-eslint",
-    parserOptions: {
-        allowImportExportEverywhere: true,
-        ecmaFeatures: {
-            jsx: true
-        }
-    },
-    env: {
-        es2017: true,
-        browser: true
     }
 }
