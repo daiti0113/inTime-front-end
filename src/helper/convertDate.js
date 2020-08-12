aba9c34376411e22d878832f080077bbe22a6453
daiti0113 <stennis.ds.0113@gmail.com>
2020-08-12 22:16:39 +0900

    fix:Fixed a bug that Taskbar shifts to the next one of dragged position

--- src/helper/convertDate.js
+++ src/helper/convertDate.js
@@ -1,6 +1,6 @@
 export const getDay = date => Number(date.replace(/^.+-/, ""))
 export const getTaskDuration = (startDate, endDate) => getDay(endDate) - getDay(startDate) + 1
-export const getDateDiff = (srcDate, dstDate) => Math.floor((dstDate.getTime() - srcDate.getTime()) / (1000 * 60 * 60 * 24))
+export const getDateDiff = (srcDate, dstDate) => Math.ceil((dstDate.getTime() - srcDate.getTime()) / (1000 * 60 * 60 * 24))
 export const formatDate = date => {
     const pre =  typeof date === "string" ? new Date(date) : date
     return `${pre.getFullYear()}-${(pre.getMonth()+1).toString().padStart(2, "0")}-${pre.getDate().toString().padStart(2, "0")}T09:00:00+09:00`
