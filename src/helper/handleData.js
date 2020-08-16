af9b28132e3f2af80f3ca7f52cb14cd6a1c59bb0
daiti0113 <stennis.ds.0113@gmail.com>
2020-08-16 23:09:34 +0900

    feat:Updated HTTP client PUT & DELETE to use firestore

--- src/helper/handleData.js
+++ src/helper/handleData.js
@@ -12,17 +12,18 @@ export const fetchData = (useEffect, path, dispatch, trigger=[]) => {
     }, trigger)
 }
 
-export const updateData = (path, dispatch, payload) => {
-    request("PUT", path, payload)
-    dispatch({type: `UPDATE_${pathToType(path)}`, payload: payload})
+export const updateData = async (path, dispatch, payload) => {
+    const response = await request("PUT", path, payload)
+    dispatch({type: `UPDATE_${pathToType(path)}`, payload: response})
 }
 
-export const createData = (path, dispatch, payload) => {
-    request("POST", path, payload)
-    dispatch({type: `CREATE_${pathToType(path)}`, payload: payload})
+export const createData = async (path, dispatch, payload) => {
+    const response = await request("POST", path, payload)
+    dispatch({type: `CREATE_${pathToType(path)}`, payload: response})
 }
 
-export const deleteData = (path, dispatch, payload) => {
+// TODO: Change all payloads to objects (Ex.use "paylod.id" in request)
+export const deleteData = async (path, dispatch, payload) => {
     request("DELETE", path, payload)
     dispatch({type: `DELETE_${pathToType(path)}`, payload: payload})
 }
--- src/helper/request.js
+++ src/helper/request.js
@@ -8,15 +8,20 @@ export const request = async (method, endpoint, param) => {
         case "GET":
             // eslint-disable-next-line no-case-declarations
             const querySnapshot = await collection.get()
-            querySnapshot.forEach(doc => res.push(doc.data()))
-            console.log(res)
+            querySnapshot.forEach(doc => res.push({...doc.data(), id: doc.id}))
             break
         case "POST":
-            res = await collection.add(param)
+            // eslint-disable-next-line no-case-declarations
+            const doc = await collection.add(param)
+            res = {...param, id: doc.id}
             break
         case "PUT":
+            await collection.doc(param.id).set(param, {merge: true})
+            res = param
             break
         case "DELETE":
+            await collection.doc(param).delete()
+            res = param
             break
     }
     return res
