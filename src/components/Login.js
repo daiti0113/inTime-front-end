fd29c8a28a549ff9319c0385b69a19e4d469a443
daiti0113 <stennis.ds.0113@gmail.com>
2020-08-21 23:47:40 +0900

    style:Updated Login page style #12

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,19 +5,32 @@ import {login} from "../helper/handleAuth"
 import {userStore} from "../stores/userStore"
 
 const useStyles = createUseStyles({
+    container: {
+        display: "grid",
+        justifyItems: "center"
+    },
+    loginForm: {
+        display: "grid",
+        gridTemplateRows: "80px 80px 30px 100px 1fr",
+        width: "100%",
+        maxWidth: "300px"
+    }
 })
 
 export const Login = () => {
+    const classes = useStyles()
     const {state: {error}, dispatch} = useContext(userStore)
     const [id, setId] = useState("")
     const [password, setPassword] = useState("")
 
     return (
-        <div>
-            {error && <div>{error.errorMessage}</div>}
-            <Input title="ID" useState={[id, setId]} />
-            <Input title="Password" useState={[password, setPassword]} type="password" />
-            <input type="button" className="paper-btn btn-secondary" value="Login" onClick={() => login(dispatch, id, password)} />
+        <div className={classes.container}>
+            <div className={classes.loginForm}>
+                <Input title="ID" useState={[id, setId]} width="100%" />
+                <Input title="Password" useState={[password, setPassword]} type="password" width="100%" />
+                <div>{error ? error.errorMessage : ""}</div>
+                <input type="button" className="paper-btn btn-secondary" value="Login" onClick={() => login(dispatch, id, password)} />
+            </div>
         </div>
     )
 }
